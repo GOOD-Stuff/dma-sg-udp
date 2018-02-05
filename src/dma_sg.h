3d7ae73b19336e3df7a9fb27f4040c295eaba904
Gustov Vladimir <gutstuf@gmail.com>
2018-02-05 16:44:05 +0300

    Some refactoring

--- src/dma_sg.h
+++ src/dma_sg.h
@@ -30,7 +30,7 @@
 #define MM2S_TAILDESC_MSB           0x14    // unused with 32bit addresses
 
 #define SG_CTL                      0x2C    // CACHE CONTROL
-#define UDP_BUFSIZE				    8192	// Size of UDP packet
+#define UDP_BUFSIZE				          8192	  // Size of UDP packet
 
 // S2MM CONTROL
 #define S2MM_CONTROL_REGISTER       0x30    // S2MM_DMACR
@@ -42,29 +42,32 @@
 
 // DESCRIPTORS FIELDS
 #define NXTDESC						0x00
-#define BUFFER_ADDRESS				0x08
+#define BUFFER_ADDRESS		0x08
 #define CONTROL						0x18
 #define STATUS						0x1C
 
 // ADDRESSES
-#define	AXI_DMA_BASEADDR            0x80400000	//AXI DMA Register Address Map
-#define	DESCRIPTOR_REGISTERS_SIZE   0xFFFF
+#define	AXI_DMA_BASEADDR            0x80400000 //AXI DMA Register Address Map
+#define	DESCRIPTOR_REGISTERS_SIZE   0x10000
 #define	SG_DMA_DESCRIPTORS_WIDTH    0x1FFFF
-#define	MEMBLOCK_WIDTH              0x1FFFFFF 	//?size? of mem used by s2mm and mm2s (32 MB)
-#define	BUFFER_BLOCK_WIDTH          0x4000      //size of memory block per descriptor in bytes (16 KB)
-#define DEST_MEM_BLOCK				0x6400000   // (100 MB)
+#define	MEMBLOCK_WIDTH              0x1FFFFFF  //?size? of mem used by s2mm and mm2s
+#define	BUFFER_BLOCK_WIDTH          0x7FFFFF   // size of memory block per descriptor in bytes
+#define DEST_MEM_BLOCK				      0x6400000 
 
-#define	HP0_DMA_BUFFER_MEM_ADDRESS         0x10000000
-#define	HP0_MM2S_DMA_BASE_MEM_ADDRESS      (HP0_DMA_BUFFER_MEM_ADDRESS)
-#define	HP0_S2MM_DMA_BASE_MEM_ADDRESS      (HP0_DMA_BUFFER_MEM_ADDRESS + MEMBLOCK_WIDTH + 1)
-#define	HP0_MM2S_DMA_DESCRIPTORS_ADDRESS   (HP0_MM2S_DMA_BASE_MEM_ADDRESS)
-#define	HP0_S2MM_DMA_DESCRIPTORS_ADDRESS   (HP0_S2MM_DMA_BASE_MEM_ADDRESS)
-#define	HP0_MM2S_SOURCE_MEM_ADDRESS        (HP0_MM2S_DMA_BASE_MEM_ADDRESS + SG_DMA_DESCRIPTORS_WIDTH + 1)
-#define	HP0_S2MM_TARGET_MEM_ADDRESS        (HP0_S2MM_DMA_BASE_MEM_ADDRESS + SG_DMA_DESCRIPTORS_WIDTH + 1)
+#define	HP0_DMA_BUFFER_MEM_ADDRESS       0x10000000
+#define	HP0_MM2S_DMA_BASE_MEM_ADDRESS    (HP0_DMA_BUFFER_MEM_ADDRESS)
+#define	HP0_S2MM_DMA_BASE_MEM_ADDRESS    (HP0_DMA_BUFFER_MEM_ADDRESS +    \
+										                      MEMBLOCK_WIDTH + 1)
+#define	HP0_MM2S_DMA_DESCRIPTORS_ADDRESS (HP0_MM2S_DMA_BASE_MEM_ADDRESS)
+#define	HP0_S2MM_DMA_DESCRIPTORS_ADDRESS (HP0_S2MM_DMA_BASE_MEM_ADDRESS)
+#define	HP0_MM2S_SOURCE_MEM_ADDRESS      (HP0_MM2S_DMA_BASE_MEM_ADDRESS + \
+										                      SG_DMA_DESCRIPTORS_WIDTH + 1)
+#define	HP0_S2MM_TARGET_MEM_ADDRESS      (HP0_S2MM_DMA_BASE_MEM_ADDRESS + \
+										                      SG_DMA_DESCRIPTORS_WIDTH + 1)
 
-void dma_reset(unsigned int *dma_vrt_address);
-void dma_descr_set(unsigned int *s2mm_descr_vrt_address, int num_descr);
-void dma_descr_scan(unsigned int *s2mm_descr_vrt_address, int num_descr);
-int udp_send(int sock, const void *buffer, const size_t buflen);
+void dma_reset     (unsigned int *dma_vrt_address);
+void dma_descr_set (unsigned int  *s2mm_descr_vrt_addr, int num_descr);
+void dma_descr_scan(unsigned int *s2mm_descr_vrt_addr, int num_descr);
+int  udp_send      (int sock, const void *buffer, const size_t buflen);
 
 #endif /* DMA_SG_H_ */
